177ea9349f1160eb5d6fe169f16ab7b7789db906
Vayu-143 <vayumishra143@gmail.com>
2026-05-29 23:34:07 +0530

    Fix Render API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://online-learning-course-recommendation-r7yl.onrender.com/api",
+    "https://online-learning-course-recommendation-r7wl.onrender.com/api",
 });
 
 export default API;
